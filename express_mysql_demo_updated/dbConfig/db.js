65ed5b739b56d7de6ac7892c1ff4331b6158de07
bibekpanta007 <bibekpanta183@gmail.com>
2026-04-08 10:38:18 -0600

    Updated DB config to use environment variables

--- express_mysql_demo_updated/dbConfig/db.js
+++ express_mysql_demo_updated/dbConfig/db.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Bibek@007',
-    database: 'volunteer_db',
+    host: process.env.db_host,
+    user: process.env.db_u,
+    password: process.env.db_p,
+    database: process.env.db_name,
+    port: process.env.db_port,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
